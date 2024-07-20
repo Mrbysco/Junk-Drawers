142b30f9539da828ef133eef5f384fe1225ccf86
Mrbysco <Mrbysco@users.noreply.github.com>
2024-07-20 14:53:05 +0200

    Enable in-game config

--- src/main/java/com/mrbysco/junkdrawers/JunkDrawers.java
+++ src/main/java/com/mrbysco/junkdrawers/JunkDrawers.java
@@ -11,6 +11,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import org.slf4j.Logger;
@@ -35,6 +37,7 @@ public class JunkDrawers {
 		eventBus.addListener(JunkRegistry::registerCapabilities);
 
 		if (dist.isClient()) {
+			container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 			eventBus.addListener(ClientHandler::onMenuRegister);
 		}
 	}
